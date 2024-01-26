6576d7271211bedb22fa8cbc7acf5be42e26960e
Danrlei <danrleidalfre@gmail.com>
2024-01-25 22:24:14 -0300

    setup design system, font and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default.ts'
+import { GlobalStyle } from './styles/global.ts'
+
 export function App() {
-  return <h1>App</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <i className="fa-brands fa-github fa-xl" />
+    </ThemeProvider>
+  )
 }
