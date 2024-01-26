4e9f7c2376ff58e9f068aceeb6c032b34a7c139a
Danrlei <danrleidalfre@gmail.com>
2024-01-25 22:55:46 -0300

    implement header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
 import { GlobalStyle } from './styles/global.ts'
+import { Home } from './pages/Home'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <i className="fa-brands fa-github fa-xl" />
+      <Home />
     </ThemeProvider>
   )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  height: 18.5rem;
+  background-image: url('/src/assets/banner.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 4rem;
+
+  img {
+    width: 150px;
+  }
+`
